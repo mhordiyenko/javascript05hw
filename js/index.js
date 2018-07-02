7012be63755a1ffcac310fdb4b4178d58465994b
Markiyan Hordiyenko <markhordi@gmail.com>
2018-07-02 13:53:27 +0000

    last fix

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
-let A = parseFloat(prompt('Введіть коефіцієнт А'));
-let B = parseFloat(prompt('Введіть коефіцієнт B'));
-let C = parseFloat(prompt('Введіть коефіцієнт C'));
+let a = parseFloat(prompt('Введіть коефіцієнт А'));
+let b = parseFloat(prompt('Введіть коефіцієнт B'));
+let c = parseFloat(prompt('Введіть коефіцієнт C'));
 
-function Discriminator(A, B, C) {
-    let D = Math.pow(B,2)-4*A*C;
-    if(D>0){
-        let x1 = (-B-Math.sqrt(D))/2*A;
-        let x2 = (-B+Math.sqrt(D))/2*A;
-        return alert('Корені рівняння x1,x2 = '+ x1 + ',' + x2);
-    } else if(D===0){
-        let x3 = (-B)/2*A;
-        return alert('Корінь рівняння x = '+ x3);
+
+function solveQuadratic(a, b, c) {
+    let d = Math.pow(b,2)-4*a*c;
+    if(d>0){
+        let x1 = (-b-Math.sqrt(d))/(2*a);
+        let x2 = (-b+Math.sqrt(d))/(2*a);
+        return 'x1 = ' +x1 +', x2 = '+x2 ;
+    } else if(d===0){
+        let x3 = (-b)/(2*a);
+        return 'x = ' +x3;
     } else{
         return alert("Квадратне рівняння немає розв'язків");
     }
 }
- let result = Discriminator(A, B, C);
+ let result = solveQuadratic(a,b,c);
+ alert(result);
