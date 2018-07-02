3470b1072ab1be75b1f52ed77ef1aac7c3bb9ad4
Markiyan Hordiyenko <markhordi@gmail.com>
2018-07-02 14:09:01 +0000

    fix

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-let a = parseFloat(prompt('Введіть коефіцієнт А'));
-let b = parseFloat(prompt('Введіть коефіцієнт B'));
-let c = parseFloat(prompt('Введіть коефіцієнт C'));
+const a = parseFloat(prompt('Введіть коефіцієнт А'));
+const b = parseFloat(prompt('Введіть коефіцієнт B'));
+const c = parseFloat(prompt('Введіть коефіцієнт C'));
 
 
 function solveQuadratic(a, b, c) {
-    let d = Math.pow(b,2)-4*a*c;
+    const d = Math.pow(b,2)-4*a*c;
     if(d>0){
-        let x1 = (-b-Math.sqrt(d))/(2*a);
-        let x2 = (-b+Math.sqrt(d))/(2*a);
+        const x1 = (-b-Math.sqrt(d))/(2*a);
+        const x2 = (-b+Math.sqrt(d))/(2*a);
         return 'x1 = ' +x1 +', x2 = '+x2 ;
     } else if(d===0){
-        let x3 = (-b)/(2*a);
+        const x3 = (-b)/(2*a);
         return 'x = ' +x3;
     } else{
         return alert("Квадратне рівняння немає розв'язків");
     }
 }
- let result = solveQuadratic(a,b,c);
+ const result = solveQuadratic(a,b,c);
  alert(result);
